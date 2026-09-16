d5b9b7ca8db37bd0f4bd4c4c3e593df143f080d6
martinaik <martinaikristensen@gmail.com>
2026-09-16 14:28:09 +0200

    Opdateret R-kode

--- opgave1.R
+++ opgave1.R
@@ -4,7 +4,9 @@ library(jsonlite)
 setwd("C:/Users/MartinaImmerkærKrist/OneDrive - Specialisterne/Opgave 1/opgave1")
 
 # Function to parse CSV text to data frame and convert to JSON
-parse_csv <- function(csv_text) {
+parse_csv <- function(csv_file) {
+  csv_text <- paste(readLines(csv_file), collapse = "\n")
+  
   lines <- strsplit(csv_text, "\n")[[1]]
   
   headers <- strsplit(lines[1], ",")[[1]]
@@ -19,15 +21,13 @@ parse_csv <- function(csv_text) {
   list(data = data, data_JSON = data_JSON)
 }
 
-# Parse CSV employees text
-employees_csv_text <- paste(readLines("employees.ascii.csv"), collapse = "\n")
-employees_data <- parse_csv(employees_csv_text)
+# Parse CSV employees.ascii.csv
+employees_data <- parse_csv("employees.ascii.csv")
 employees_data_frame <- employees_data$data
 employees_JSON <- employees_data$data_JSON
 
-# Parse CSV sogne text
-sogne_csv_text <- paste(readLines("sogne.dawa.csv"), collapse = "\n")
-sogne_data <- parse_csv(sogne_csv_text)
+# Parse CSV sogne.dawa.csv
+sogne_data <- parse_csv("sogne.dawa.csv")
 sogne_data_frame <- sogne_data$data
 sogne_JSON <- sogne_data$data_JSON
 
