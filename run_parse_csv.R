31da5c802e76770de6e2b2e4b622981b7e4960e7
martinaik <martinaikristensen@gmail.com>
2026-09-23 09:48:50 +0200

    Opdateret kode

--- run_parse_csv.R
+++ run_parse_csv.R
@@ -3,8 +3,9 @@
 source("parse_csv.R")
 
 csv_file <- readline(prompt = "Enter the name of the CSV file: ")
+header_input <- readline(prompt = "Does the CSV file contain a header? (TRUE/FALSE): ")
 
-result <- parse_csv(csv_file)
+result <- parse_csv(csv_file, header_input)
 
 print(result$dataframe)
 cat(result$data_JSON)
